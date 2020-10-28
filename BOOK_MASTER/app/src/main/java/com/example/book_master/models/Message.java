deeea41e4449f0a45c675068e7b4052c3e642687
jack <jiatian1@ualberta.ca>
2020-10-27 18:51:38 -0600

    meaniful commit

--- BOOK_MASTER/app/src/main/java/com/example/book_master/models/Message.java
+++ BOOK_MASTER/app/src/main/java/com/example/book_master/models/Message.java
@@ -1,4 +1,16 @@
 package com.example.book_master.models;
 
+import java.util.ArrayList;
+
 public class Message {
+    private ArrayList<String> messageList;
+
+    public void Add_New_Message(){
+
+    }
+
+    public void Delete_message(){
+
+    }
 }
+
