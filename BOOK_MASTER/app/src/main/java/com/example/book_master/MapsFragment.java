70412efcccc72b6efb0afff14927f15b9edb1149
Rubin Won <55333006+ruby199@users.noreply.github.com>
2020-11-28 21:07:25 -0700

    MapFrag

--- BOOK_MASTER/app/src/main/java/com/example/book_master/MapsFragment.java
+++ BOOK_MASTER/app/src/main/java/com/example/book_master/MapsFragment.java
@@ -39,7 +39,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_maps, container,false);
+        view = inflater.inflate(R.layout.geo_to_deliver, container,false);
         return view;
     }
     @Override
